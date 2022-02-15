80cc9e76ee54c7a06df01e63b1837a86784d09df
h-y-e-j-i <11th_hj@naver.com>
2022-02-15 15:37:49 +0900

    not finish

--- backjoon/6987.py
+++ backjoon/6987.py
@@ -2,7 +2,12 @@
 # 월드컵
 import sys
 
-answers = list()
+game_count = 0
+
+def DFS_backtracking():
+    if game_count == 15 :
+        return 1
+    
 
 team_name = ['A', 'B', 'C', 'D', 'E', 'F']
 
@@ -16,36 +21,7 @@ for _ in range(4):
         draw_results.append(input_result[3*i+1])
         lose_results.append([team_name[i], input_result[3*i+2]]) # 패만 누적
 
-    win_results = sorted(win_results, key=lambda x:x[1], reverse=True) # 승 내림차순 정렬
-    lose_results = sorted(lose_results, key=lambda x:x[1], reverse=True) # 패 기준으로 내림차순으로 정렬
+    DFS_backtracking()
 
-    if sum(draw_results) %2 != 0 or  \
-        sum([lose_score for _, lose_score in lose_results])!=sum([win_score for _, win_score in win_results]):
-        ans = 0
-    else:
-        for win_team_name, win_score in win_results:
-            lose_team_count = 0
-            for lose_team_name, lose_socre in lose_results:
-                if win_team_name!=lose_team_name and lose_socre>0 : lose_team_count += 1
-            
-            # 이긴 팀의 승 횟수가 진 횟수를 차감할 수 있는 팀보다 같거나 적으면
-            # 진 팀의 진 횟수를 차감한다
-            if win_score <= lose_team_count:
-                lose_team_count = win_score
-                for lose_index in range(len(lose_results)):
-                    lose_team_name, lose_socre = lose_results[lose_index][0], lose_results[lose_index][1]
 
-                    if win_team_name!=lose_team_name and lose_results[lose_index][1]>0 : lose_results[lose_index][1] -=1
-                    
-                    lose_team_count -=1
-                    if lose_team_count == 0 : break
 
-                lose_results = sorted(lose_results, key=lambda x:x[1], reverse=True) # 패 기준으로 내림차순으로 정렬
-            # 그렇지 않으면 차감할 수 없으므로
-            # 0 출력
-            else : 
-                ans = 0
-                break
-    answers.append(ans)
-
-for ans in answers : print(ans, end=' ')
