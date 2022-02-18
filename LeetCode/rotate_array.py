d5ab9e11cd20b5fb5982ce3ccd2a251c55bbc5f6
h-y-e-j-i <11th_hj@naver.com>
2022-02-19 02:14:49 +0900

    0219 boostcamp

--- LeetCode/rotate_array.py
+++ LeetCode/rotate_array.py
@@ -1,8 +1,4 @@
 class Solution(object):
     def rotate(self, nums, k):
-        right_list = nums[len(nums)-k:]
-        left_list = nums[:len(nums)-k]
-        return right_list+left_list
-
-solution = Solution()
-print(solution.rotate([1,2,3,4,5,6,7], 3))
+        k = k%len(nums)
+        nums[:] = nums[len(nums)-k:] + nums[:len(nums)-k]
--- backjoon/7576.py
+++ backjoon/7576.py
@@ -2,34 +2,51 @@
 # 토마토
 
 import sys
+from collections import deque
 
-tomato_grpah = dict()
-ripe_tomatoes = list() # 익은 토마토 위치
+queue = deque()
+visit = list()
+
+dxs = [1, -1, 0, 0]
+dys = [0, 0, 1, -1]
+
+ripping = False
+ripe_day = 0
 
 size_M, size_N = list(map(int, sys.stdin.readline().strip().split()))
 tomato_box = list()
 for _ in range(size_N):
     tomato_box.append(list(map(int, sys.stdin.readline().strip().split())))
 
-for i in range(size_M):
-    for j in range(size_N):
-        tomato_grpah[f'{i} {j}'] = list()
-        if tomato_box[i][j] in [0, 1]:
-            if tomato_box[i][j] == 1 : ripe_tomatoes.append(f"{i} {j}")
-
-            if i-1>=0 and tomato_box[i-1][j] == 0:
-                tomato_grpah[f'{i} {j}'].append(f"{i-1} {j}")
-            if i+1<size_M and tomato_box[i-1][j] == 0:
-                tomato_grpah[f'{i} {j}'].append(f"{i+1} {j}")
-            if j-1>=0 and tomato_box[i][j-1] == 0:
-                tomato_grpah[f'{i} {j}'].append(f"{i} {j-1}")
-            if j+1<size_N and tomato_box[i][j-1] == 0:
-                tomato_grpah[f'{i} {j}'].append(f"{i} {j+1}")
-
-if any([len(val) for val in tomato_box.values()]) == False:
-    print(-1)
-else:
-    # 
+for i in range(size_N):
+    for j in range(size_M): 
+        if tomato_box[i][j] == 1: queue.append([i, j])       
 
 
+while queue:
+    node = queue.popleft()
+    tomato_x, tomato_y = node
 
+    for dx, dy in zip(dxs, dys):
+        new_tomato_x, new_tomato_y = tomato_x+dx, tomato_y+dy
+        if new_tomato_x>=0 and new_tomato_x<size_N \
+            and new_tomato_y>=0 and new_tomato_y<size_M \
+            and tomato_box[new_tomato_x][new_tomato_y] == 0:
+            tomato_box[new_tomato_x][new_tomato_y] = tomato_box[tomato_x][tomato_y]+1
+            queue.append([new_tomato_x, new_tomato_y])
+            ripping = True
+
+        if ripping:
+            ripe_day = tomato_box[tomato_x][tomato_y]
+            ripping = False
+
+full_tomato = True
+for i in range(size_N):
+    for j in range(size_M): 
+        if tomato_box[i][j] == 0 : 
+            full_tomato = False
+            break
+
+if not full_tomato : print(-1)
+else : print(ripe_day)
+                        
