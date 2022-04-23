153cdc968c5e331c725d4ee64f329ed65f40eda9
h-y-e-j-i <11th_hj@naver.com>
2022-04-23 23:52:40 +0900

    숏코딩

--- SW Expert Academy/1208.py
+++ SW Expert Academy/1208.py
@@ -2,11 +2,8 @@
 # Flatten
 
 for test_case in range(1, 11):
-    dump_count = int(input())
-    yellow_boxes = list(map(int, input().strip().split()))
-
+    dump_count, yellow_boxes = int(input()), list(map(int, input().strip().split()))
     for _ in range(dump_count):
         yellow_boxes[yellow_boxes.index(max(yellow_boxes))] -= 1
         yellow_boxes[yellow_boxes.index(min(yellow_boxes))] += 1
-
     print(f"#{test_case} {max(yellow_boxes)-min(yellow_boxes)}")
