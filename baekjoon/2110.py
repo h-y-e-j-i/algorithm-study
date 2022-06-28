16f301ab4c372fbe1c5028ddfb9355c0129b627d
h-y-e-j-i <11th_hj@naver.com>
2022-06-29 02:03:40 +0900

    not finish

---
+++ baekjoon/2110.py
@@ -0,0 +1,34 @@
+# https://www.acmicpc.net/problem/2110
+# 공유기 설치
+# 이분탐색
+# https://my-coding-notes.tistory.com/119
+
+N, C = map(int, input().split(" "))
+
+home = list()
+for _ in range(N) : home.append(int(input()))
+home = sorted(home)
+
+start, end = 1, home[-1]-home[0]
+
+# 공유기의 숫자를 만족할 때 까지 집 사이의 간격으로 이분 탐색
+while start<=end:
+    mid = (start+end)//2
+
+    wifi_count = 1
+    home_wifi = home[0] # 공유기를 마지막으로 설치한 집
+    for i in range(N):
+        # 마지막으로 설치한 집과 설치할 집의 간격이 최소 간격이라면 추가 설치
+        if home[i]-home_wifi>mid :
+            wifi_count += 1
+            home_wifi = home[i]
+            if wifi_count >= C : break
+    
+    if wifi_count >= C : 
+        result = mid
+        start = mid
+    else : end = mid-1
+
+print(result)
+
+
