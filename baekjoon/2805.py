ecd0e03d2dfbb5df811def43bb1ad2ace23aba4f
h-y-e-j-i <11th_hj@naver.com>
2022-06-29 01:06:36 +0900

    시간 초과 해결

--- baekjoon/2805.py
+++ baekjoon/2805.py
@@ -3,19 +3,19 @@
 # https://developoerty.tistory.com/31
 import sys
 
-N, M = list(map(int, sys.stdin.readline().strip().split(" ")))
-tree_height = list(map(int, sys.stdin.readline().strip().split(" ")))
-start, end = 1, max(tree_height)
+N, M = map(int, input().split(" "))
+tree_height = list(map(int,  input().split(" ")))
+start, end = 0, max(tree_height)
 
 while start <= end:
     mid = (start+end)//2
-    log = list()
+    log_sum = 0
     for h in tree_height:
-        diff = h-mid    
-        if diff < 0 : log.append(0)
-        else : log.append(diff)
+        if h > mid : log_sum += (h-mid)
+        # diff = h-mid    
+        # if diff > 0 : log_sum += diff
     
-    if sum(log)>=M : start = mid+1
+    if log_sum>=M : start = mid+1
     else : end = mid-1
 print(end)
     
