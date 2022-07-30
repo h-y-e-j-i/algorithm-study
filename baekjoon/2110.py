24744387819d905dd571ef48bfe7e329784c1529
h-y-e-j-i <11th_hj@naver.com>
2022-07-31 02:22:40 +0900

    0730

--- baekjoon/2110.py
+++ baekjoon/2110.py
@@ -1,12 +1,13 @@
 # https://www.acmicpc.net/problem/2110
 # 공유기 설치
 # 이분탐색
-# https://my-coding-notes.tistory.com/119
+# https://dundung.tistory.com/54
+import sys
 
-N, C = map(int, input().split(" "))
+N, C = map(int, sys.stdin.readline().strip())
 
 home = list()
-for _ in range(N) : home.append(int(input()))
+for _ in range(N) : home.append(int(sys.stdin.readline().strip()))
 home = sorted(home)
 
 start, end = 1, home[-1]-home[0]
@@ -19,14 +20,14 @@ while start<=end:
     home_wifi = home[0] # 공유기를 마지막으로 설치한 집
     for i in range(N):
         # 마지막으로 설치한 집과 설치할 집의 간격이 최소 간격이라면 추가 설치
-        if home[i]-home_wifi>mid :
+        if home[i]-home_wifi>=mid :
             wifi_count += 1
             home_wifi = home[i]
-            if wifi_count >= C : break
+        if wifi_count >= C : break
     
     if wifi_count >= C : 
         result = mid
-        start = mid
+        start = mid+1
     else : end = mid-1
 
 print(result)
