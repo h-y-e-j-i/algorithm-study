f0e65bf46f2517ff66c56ca8aa4ccfc427c733d0
h-y-e-j-i <11th_hj@naver.com>
2022-08-01 22:15:14 +0900

    0801

---
+++ programmers/select1.sql
@@ -0,0 +1,5 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59034
+-- 모든 레코드 조회하기
+-- SELECT
+
+SELECT * FROM ANIMAL_INS ORDER BY ANIMAL_ID
---
+++ programmers/select4.sql
@@ -0,0 +1,5 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59037?language=mysql
+-- 어린 동물 찾기
+-- SELECT
+
+SELECT ANIMAL_ID, NAME FROM ANIMAL_INS WHERE INTAKE_CONDITION<>'Aged' ORDER BY ANIMAL_ID
