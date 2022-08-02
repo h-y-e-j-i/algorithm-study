fb28dafafe10b7863a0b85ce20c937348ec441f1
h-y-e-j-i <11th_hj@naver.com>
2022-08-02 19:25:48 +0900

    파일 삭제

--- programmers/String_Date6.sql
+++
@@ -1,9 +0,0 @@
--- https://school.programmers.co.kr/learn/courses/30/lessons/59414
--- DATETIME에서 DATE로 형 변환
--- String, Date
-/*
-    date_format() 사용하기
-    https://lightblog.tistory.com/155
-*/
-
-SELECT ANIMAL_ID, NAME, date_format(DATETIME, "%Y-%m-%d") FROM ANIMAL_INS ORDER BY ANIMAL_ID
