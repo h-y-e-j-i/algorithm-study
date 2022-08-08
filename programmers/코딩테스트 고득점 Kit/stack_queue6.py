836d7600dac9302df1d6fa110cb6ad5d572653c1
h-y-e-j-i <11th_hj@naver.com>
2022-08-08 23:15:03 +0900

    0808

---
+++ programmers/코딩테스트 고득점 Kit/stack_queue6.py
@@ -0,0 +1,17 @@
+def solution(s):
+    s_list = list(map(str, s))
+
+    stack = list()
+
+    for se in s_list:
+        if se == "(" : stack.append(")")
+        else : 
+            if not stack : return False
+            else : stack.pop()
+    
+    if stack : return False
+    else : return True
+
+# print(solution("()()"))
+# print(solution("(())()"))
+print(solution("(()("))
