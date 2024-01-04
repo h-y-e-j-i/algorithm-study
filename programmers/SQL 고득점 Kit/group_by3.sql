6663f221d98c5779fc36c059f7c282dd5d08c1e2
h-y-e-j-i <hyeji4e@gmail.com>
2024-01-05 00:00:21 +0900

    Remove: 프로그래머스 SQL 고득점 Kit - 기존 파일 삭제

--- programmers/SQL 고득점 Kit/group_by3.sql
+++
@@ -1,5 +0,0 @@
--- https://school.programmers.co.kr/learn/courses/30/lessons/59041
--- 동명 동물 수 찾기
--- GROUP BY
-
-SELECT NAME, COUNT(NAME) FROM ANIMAL_INS  GROUP BY NAME HAVING COUNT(NAME)>=2 ORDER BY NAME
