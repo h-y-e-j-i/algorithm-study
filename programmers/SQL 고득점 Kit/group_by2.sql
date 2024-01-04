443a33a9bf12a68f33f7c05f46dcd6b4412b92c4
h-y-e-j-i <hyeji4e@gmail.com>
2024-01-05 00:00:07 +0900

    Remove: 프로그래머스 SQL 고득점 Kit - 기존 파일 삭제

--- programmers/SQL 고득점 Kit/group_by2.sql
+++
@@ -1,5 +0,0 @@
--- https://school.programmers.co.kr/learn/courses/30/lessons/59040
--- 고양이와 개는 몇 마리 있을까
--- GROUP BY
-
-SELECT ANIMAL_TYPE, COUNT(ANIMAL_TYPE) FROM ANIMAL_INS WHERE ANIMAL_TYPE="cat" or ANIMAL_TYPE="Dog" GROUP BY ANIMAL_TYPE ORDER BY ANIMAL_TYPE
